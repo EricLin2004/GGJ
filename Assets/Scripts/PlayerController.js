fc298771b34b7c9e47a38d273a878ef4668749d9
Marc Kuo <kuomarc2@gmail.com>
2014-01-25 13:52:32 -0800

    Dash with space -- but still flies through walls!

--- Assets/Scripts/PlayerController.js
+++ Assets/Scripts/PlayerController.js
@@ -1,6 +1,8 @@
 ﻿#pragma strict
 
 var speed : float;
+var dash : float;
+var staticDash : float;
 
 function Start () {
 
@@ -14,7 +16,13 @@ function FixedUpdate() {
 	var moveHorizontal : float = Input.GetAxis("Horizontal");
 	var moveVertical : float = Input.GetAxis("Vertical");
 	
+	var speedX : float = speed;
+	if (Input.GetButtonDown("Jump")) {
+	  print("check");
+	  speedX = (speed + staticDash) * dash;
+	}
+	
 	var movement : Vector3 = new Vector3(moveHorizontal,0,moveVertical);
 	
-	rigidbody.AddForce(movement * speed * Time.deltaTime);
+	rigidbody.AddForce(movement * speedX * Time.deltaTime);
 }
