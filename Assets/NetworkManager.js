62b1cdd73565ebd497180b8f3821c0169393a2dc
Marc Kuo <kuomarc2@gmail.com>
2014-01-25 17:34:15 -0800

    Basic server/client host/join

---
+++ Assets/NetworkManager.js
@@ -0,0 +1,46 @@
+﻿#pragma strict
+
+var typeName : String = "BallOnBall";
+var gameName : String = "Arena";
+var hostList : HostData[];
+
+function StartServer(){
+  Network.InitializeServer(4, 25000, !Network.HavePublicAddress());
+  MasterServer.RegisterHost(typeName, gameName);
+}
+
+function OnServerInitialized () {
+	Debug.Log("Server Initialized!");
+}
+
+function OnGUI() {
+	if (!Network.isClient && !Network.isServer) {
+		if (GUI.Button(new Rect(100, 100, 250, 100), "Start Server"))
+			StartServer();
+		if (GUI.Button(new Rect(100, 250, 250, 100), "Refresh Hosts"))
+            RefreshHostList();
+        if (hostList != null) {
+            for (var i : int = 0; i < hostList.Length; i++) {
+                if (GUI.Button(new Rect(400, 100 + (110 * i), 300, 100), hostList[i].gameName))
+                    JoinServer(hostList[i]);
+            }
+        }
+	}
+}
+
+function RefreshHostList () {
+	MasterServer.RequestHostList(typeName);
+}
+
+function OnMasterServerEvent(msEvent : MasterServerEvent){
+  if (msEvent == MasterServerEvent.HostListReceived)
+    hostList = MasterServer.PollHostList();
+}
+
+function JoinServer(hostData : HostData){
+  Network.Connect(hostData);
+}
+
+function OnConnectedToServer(){
+  Debug.Log("Server Joined!");
+}
