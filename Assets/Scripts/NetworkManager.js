9f71178f73d134e26379bdb7bbda73162dc15557
Marc Kuo <kuomarc2@gmail.com>
2014-01-26 15:05:22 -0800

    basic splash screen with buttons

--- Assets/Scripts/NetworkManager.js
+++ Assets/Scripts/NetworkManager.js
@@ -4,7 +4,11 @@ var typeName : String = "BallOnBall";
 var gameName : String = "Arena";
 var hostList : HostData[];
 var playerPrefab : GameObject;
+var customButton : GUIStyle;
 var background : Texture;
+var host : Texture;
+var refresh : Texture;
+var join : Texture;
 
 function StartServer(){
   Network.InitializeServer(4, 25000, !Network.HavePublicAddress());
@@ -20,13 +24,13 @@ function OnServerInitialized () {
 function OnGUI() {
 	if (!Network.isClient && !Network.isServer) {
 		GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), background, ScaleMode.StretchToFill);
-		if (GUI.Button(new Rect(100, 100, 250, 100), "Start Server"))
+		if (GUI.Button(new Rect(Screen.width * 0.25, Screen.height * 0.65, Screen.width * 0.5, Screen.height * 0.15), host, customButton))
 			StartServer();
-		if (GUI.Button(new Rect(100, 250, 250, 100), "Refresh Hosts"))
+		if (GUI.Button(new Rect(Screen.width * 0.41, Screen.height * 0.8, Screen.width * 0.2, Screen.height * 0.05), refresh, customButton))
             RefreshHostList();
         if (hostList != null) {
             for (var i : int = 0; i < hostList.Length; i++) {
-                if (GUI.Button(new Rect(400, 100 + (110 * i), 300, 100), hostList[i].gameName))
+                if (GUI.Button(new Rect(Screen.width * 0.25, Screen.height * 0.9, Screen.width * 0.5, Screen.height * 0.15), join, customButton))
                     JoinServer(hostList[i]);
             }
         }
