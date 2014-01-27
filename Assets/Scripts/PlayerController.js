a179b52f98eaefad10ca664c9a9decb31fc452c1
Marc Kuo <kuomarc2@gmail.com>
2014-01-26 16:53:11 -0800

    Add Collision between players and destroy sound

--- Assets/Scripts/PlayerController.js
+++ Assets/Scripts/PlayerController.js
@@ -3,6 +3,7 @@
 var speed : float;
 var dash : float;
 var dashSound : AudioClip;
+var explodeSound : AudioClip;
 
 function Start () {
 
@@ -29,5 +30,19 @@ function FixedUpdate() {
 		var movement : Vector3 = new Vector3(moveHorizontal,0,moveVertical);
 		
 		rigidbody.AddForce(movement * speed * Time.deltaTime);
+	}
+}
+
+function OnCollisionEnter(other : Collision) {
+	
+	if(other.gameObject.tag == "Player") {
+		AudioSource.PlayClipAtPoint(explodeSound, transform.position);
+		if(other.gameObject.rigidbody.velocity.magnitude > rigidbody.velocity.magnitude) {
+			print("game over!");		
+			gameObject.SetActive(false);
+		} else {
+			print("You win!");
+			other.gameObject.SetActive(false);
+		}
 	}
 }
